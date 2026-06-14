55468c598901d4a773c9439b42139042592407ab
Kristoffer Dalby <kristoffer@dalby.cc>
2026-06-14 10:46:13 +0200

    ci: run checks via kradalby/flake-checks

--- flake.nix
+++ flake.nix
@@ -4,119 +4,145 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    flake-checks.url = "github:kradalby/flake-checks";
+    flake-checks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    ...
-  }: let
-    tasmota-exporterVersion =
-      if (self ? shortRev)
-      then self.shortRev
-      else "dev";
-  in
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , flake-checks
+    , ...
+    }:
+    let
+      tasmota-exporterVersion =
+        if (self ? shortRev)
+        then self.shortRev
+        else "dev";
+    in
     {
-      overlays.default = _: prev: let
-        pkgs = nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system};
-      in {
-        tasmota-exporter = pkgs.callPackage ({buildGo126Module}:
-          buildGo126Module {
-            pname = "tasmota-exporter";
-            version = tasmota-exporterVersion;
-            src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+      overlays.default = _: prev:
+        let
+          pkgs = nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system};
+        in
+        {
+          tasmota-exporter = pkgs.callPackage
+            ({ buildGo126Module }:
+              buildGo126Module {
+                pname = "tasmota-exporter";
+                version = tasmota-exporterVersion;
+                src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
 
-            subPackages = ["cmd/tasmota-exporter"];
+                subPackages = [ "cmd/tasmota-exporter" ];
 
-            vendorHash = "sha256-bZqg+XnuEtWfja7C2OD9wZuNwYRkJ8b9kxyTdghcMlE=";
-          }) {};
-      };
+                vendorHash = "sha256-bZqg+XnuEtWfja7C2OD9wZuNwYRkJ8b9kxyTdghcMlE=";
+              })
+            { };
+        };
     }
     // utils.lib.eachDefaultSystem
-    (system: let
-      pkgs = import nixpkgs {
-        overlays = [self.overlays.default];
-        inherit system;
-      };
-      buildDeps = with pkgs; [
-        git
-        gnumake
-        go_1_26
-      ];
-      devDeps = with pkgs;
-        buildDeps
-        ++ [
-          golangci-lint
-          entr
-        ];
-    in {
-      # `nix develop`
-      devShells.default = pkgs.mkShell {
-        buildInputs = devDeps;
-      };
-
-      # `nix build`
-      packages = with pkgs; {
-        inherit tasmota-exporter;
-        default = tasmota-exporter;
-      };
-
-      # `nix run`
-      apps = {
-        tasmota-exporter = utils.lib.mkApp {
-          drv = pkgs.tasmota-exporter;
+      (system:
+      let
+        pkgs = import nixpkgs {
+          overlays = [ self.overlays.default ];
+          inherit system;
         };
-        default = utils.lib.mkApp {
-          drv = pkgs.tasmota-exporter;
+        fc = flake-checks.lib;
+        common = {
+          inherit pkgs;
+          root = ./.;
+          pname = "tasmota-exporter";
+          version = tasmota-exporterVersion;
+          vendorHash = "sha256-bZqg+XnuEtWfja7C2OD9wZuNwYRkJ8b9kxyTdghcMlE=";
+          goPkg = pkgs.go_1_26;
         };
-      };
-    })
+      in
+      {
+        # `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            gnumake
+            go_1_26
+            golangci-lint
+            entr
+          ];
+        };
+
+        formatter = fc.formatter common;
+
+        # `nix flake check`
+        checks = {
+          build = fc.goBuild common;
+          gotest = fc.goTest common;
+          golangci-lint = fc.goLint common;
+          formatting = fc.goFormat common;
+        };
+
+        # `nix build`
+        packages = with pkgs; {
+          inherit tasmota-exporter;
+          default = tasmota-exporter;
+        };
+
+        # `nix run`
+        apps = {
+          tasmota-exporter = utils.lib.mkApp {
+            drv = pkgs.tasmota-exporter;
+          };
+          default = utils.lib.mkApp {
+            drv = pkgs.tasmota-exporter;
+          };
+        };
+      })
     // {
-      nixosModules.default = {
-        pkgs,
-        lib,
-        config,
-        ...
-      }: let
-        cfg = config.services.tasmota-exporter;
-      in {
-        options = with lib; {
-          services.tasmota-exporter = {
-            enable = mkEnableOption "Enable tasmota-exporter";
+      nixosModules.default =
+        { pkgs
+        , lib
+        , config
+        , ...
+        }:
+        let
+          cfg = config.services.tasmota-exporter;
+        in
+        {
+          options = with lib; {
+            services.tasmota-exporter = {
+              enable = mkEnableOption "Enable tasmota-exporter";
 
-            package = mkOption {
-              type = types.package;
-              description = ''
-                tasmota-exporter package to use
+              package = mkOption {
+                type = types.package;
+                description = ''
+                  tasmota-exporter package to use
+                '';
+                default = pkgs.tasmota-exporter;
+              };
+
+              listenAddr = mkOption {
+                type = types.str;
+                default = ":9090";
+              };
+            };
+          };
+          config = lib.mkIf cfg.enable {
+            systemd.services.tasmota-exporter = {
+              enable = true;
+              script = ''
+                export TASMOTA_EXPORTER_LISTEN_ADDR=${cfg.listenAddr}
+                ${cfg.package}/bin/tasmota-exporter
               '';
-              default = pkgs.tasmota-exporter;
-            };
-
-            listenAddr = mkOption {
-              type = types.str;
-              default = ":9090";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              serviceConfig = {
+                DynamicUser = true;
+                Restart = "always";
+                RestartSec = "15";
+              };
+              path = [ cfg.package ];
             };
           };
         };
-        config = lib.mkIf cfg.enable {
-          systemd.services.tasmota-exporter = {
-            enable = true;
-            script = ''
-              export TASMOTA_EXPORTER_LISTEN_ADDR=${cfg.listenAddr}
-              ${cfg.package}/bin/tasmota-exporter
-            '';
-            wantedBy = ["multi-user.target"];
-            after = ["network-online.target"];
-            wants = ["network-online.target"];
-            serviceConfig = {
-              DynamicUser = true;
-              Restart = "always";
-              RestartSec = "15";
-            };
-            path = [cfg.package];
-          };
-        };
-      };
     };
 }
